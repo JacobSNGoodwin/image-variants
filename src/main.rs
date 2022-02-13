503d9b563726e09dbcb2143f5bd185b47c40093c
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-02-12 20:44:43 -0800

    get all types from previous node version

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,33 @@ use clap::{ArgEnum, Parser};
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// The relative directory containing the images
-    #[clap(short, long)]
-    dir: Option<String>,
+    #[clap(short, long, default_value_t = String::from(""))]
+    dir: String,
 
-    // extention types to convert into
-    #[clap(short, long, arg_enum)]
-    ext: Vec<OutputTypes>,
+    /// The relative directory containing the images
+    #[clap(short, long, default_value_t = String::from("variants"))]
+    out_dir: String,
+
+    /// Space-separated list of output file-types
+    #[clap(short, long, arg_enum, multiple_values(true))]
+    formats: Option<Vec<OutputTypes>>,
+
+    /// Space-separated list of image variant widths in pixels
+    #[clap(short, long, multiple_values(true))]
+    widths: Option<Vec<u16>>,
+
+    /// Do not include a low-quality image placeholder
+    #[clap(long)]
+    no_lqip: bool,
+
+    /// The outout quality for JPG and WEBP images.
+    /// Should be a value from 1-100.
+    #[clap(short, long, default_value_t = 80, validator(quality_range))]
+    quality: u8,
+
+    /// Only include a low-quality image placeholder
+    #[clap(long)]
+    lqip_only: bool,
 }
 
 #[derive(ArgEnum, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -22,12 +43,33 @@ enum OutputTypes {
     SVG,
 }
 
+fn quality_range(v: &str) -> Result<(), String> {
+    if let Ok(val) = v.parse::<u8>() {
+        if val >= 1 && val <= 100 {
+            return Ok(());
+        }
+
+        Err(String::from(
+            "quality must be an integer value between 0 and 100",
+        ))
+    } else {
+        Err(String::from(
+            "quality must be an integer value between 0 and 100",
+        ))
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
-    let dir = args.dir.unwrap_or(String::from(""));
+    println!("{:?}", args);
 
-    println!("The dir is \"{}\"", dir);
+    let formats = args
+        .formats
+        .unwrap_or(vec![OutputTypes::JPG, OutputTypes::WEBP]);
 
-    println!("The output types are \"{:?}\"", args.ext);
+    let widths = args.widths.unwrap_or(vec![800, 1200, 1800, 2400]);
+
+    println!("The output types are \"{:?}\"", formats);
+    println!("The widths are \"{:?}\"", widths);
 }
