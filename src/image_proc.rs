fb3b020bd8db52dc631a09fb6245f1b9cbab5a52
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-02-19 10:34:03 -0800

    screw around with custom error for image_proc

--- src/image_proc.rs
+++ src/image_proc.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt::Display, io, path::Path};
 
 use image::GenericImageView;
 
@@ -9,28 +9,55 @@ pub struct LQIPData {
     height: u32,
 }
 
-pub fn create_lqip(input: &Path) -> Option<LQIPData> {
-    let img = match image::open(input) {
-        Ok(img) => img,
-        Err(_) => return None,
-    };
+#[derive(Debug)]
+enum ImageProcError {
+    Conversion,
+    IO(io::Error),
+}
 
-    let ext = match input.extension() {
-        Some(ext) => ext,
-        None => return None,
-    };
+impl Display for ImageProcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ImageProcError::Conversion => write!(f, "There was an error converting the image."),
+            ImageProcError::IO(e) => write!(f, "IO error: {}", e),
+        }
+    }
+}
 
-    let ext_str = match ext.to_str() {
-        Some(s) => s,
-        None => return None,
-    };
+impl From<image::ImageError> for ImageProcError {
+    fn from(e: image::ImageError) -> Self {
+        match e {
+            image::ImageError::Decoding(_) => ImageProcError::Conversion,
+            image::ImageError::Encoding(_) => ImageProcError::Conversion,
+            image::ImageError::Parameter(_) => ImageProcError::Conversion,
+            image::ImageError::Limits(_) => ImageProcError::Conversion,
+            image::ImageError::Unsupported(_) => ImageProcError::Conversion,
+            image::ImageError::IoError(e) => ImageProcError::IO(e),
+        }
+    }
+}
+
+pub type ImageProcResult<T> = Result<T, ImageProcError>;
+
+pub fn create_lqip(input: &Path) -> ImageProcResult<LQIPData> {
+    let img = image::open(input)?;
+
+    let ext = input.extension().ok_or(ImageProcError::IO(io::Error::new(
+        io::ErrorKind::Other,
+        "Could not extract file extension",
+    )))?;
+
+    let ext_str = ext.to_str().ok_or(ImageProcError::IO(io::Error::new(
+        io::ErrorKind::Other,
+        "Could not extract file extension",
+    )))?;
 
     let (width, height) = img.dimensions();
     let lqip_img = img.resize(30, 30, image::imageops::Nearest).blur(5.0);
     let img_buf = lqip_img.as_bytes();
     let as_base64 = base64::encode(&img_buf);
 
-    Some(LQIPData {
+    Ok(LQIPData {
         image: format!("data:image/{};base64,{}", ext_str, as_base64),
         width,
         height,
